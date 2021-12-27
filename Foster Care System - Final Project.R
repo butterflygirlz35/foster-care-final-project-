c4c07e1aa2c567579569c5603d08cccc91b4197c
doug9427 <95145385+doug9427@users.noreply.github.com>
2021-12-27 09:40:04 -0600

    Add files via upload
    Here is how I was able to wrangle the data.

--- Foster Care System - Final Project.R
+++ Foster Care System - Final Project.R
@@ -14,10 +14,13 @@ Exiting_age = names(Exiting_fc)[names(Exiting_fc) == "Age group"] <- "AgeGroup"
 
 # Subsetting United States' Total data
 Exiting_USA = Exiting_fc %>% filter((Location == 'United States') & (AgeGroup == 'Total') & (TimeFrame %in% c('2001', '2003', '2006', '2007', '2010', '2011', '2013', '2015', '2017', '2019')))
+Exiting_USA = Exiting_USA %>% mutate(Data = as.numeric(Data))
+Exiting_USA1 = aggregate(Data~Location, Exiting_USA, sum)
 
 # Subsetting the 10 States' data
 Exiting_States = Exiting_fc %>% filter((Location %in% c('Arizona', 'California', 'Colorado', 'Connecticut', 'Georgia', 'Indiana', 'New Jersey', 'Ohio', 'Texas', 'Utah', 'Vermont')) & (TimeFrame %in% c('2001', '2003', '2006', '2007', '2010', '2011', '2013', '2015', '2017', '2019')))
-
+Exiting_States = Exiting_States %>% mutate(Data = as.numeric(Data))
+Exiting_States1 = aggregate(Data~Location + AgeGroup, Exiting_States, sum)
 
 ## Youth incarceration dataset
 Juveniles = Youth_residing_in_juvenile_detention_correctional_and_or_residential_facilities
@@ -27,6 +30,10 @@ Juveniles_IN = select(Juveniles, c('Location', 'TimeFrame', 'Data'))
 
 # Subsetting United States' Total data
 Juveniles_USA = Juveniles_IN %>% filter((Location == 'United States') & (TimeFrame %in% c('2001', '2003', '2006', '2007', '2010', '2011', '2013', '2015', '2017', '2019')))
+Juveniles_USA = Juveniles_USA %>% mutate(Data = as.numeric(Data))
+Juveniles_USA1 = aggregate(Data~Location, Juveniles_States, sum)
 
 # Subsetting the 10 States' data
 Juveniles_States = Juveniles_IN %>% filter((Location %in% c('Arizona', 'California', 'Colorado', 'Connecticut', 'Georgia', 'Indiana', 'New Jersey', 'Ohio', 'Texas', 'Utah', 'Vermont')) & (TimeFrame %in% c('2001', '2003', '2006', '2007', '2010', '2011', '2013', '2015', '2017', '2019')))
+Juveniles_States = Juveniles_States %>% mutate(Data = as.numeric(Data))
+Juveniles_States1 = aggregate(Data~Location, Juveniles_States, sum)
