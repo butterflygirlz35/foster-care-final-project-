d24a09349517f0b2392ee38be7ad892c22a8ce41
doug9427 <95145385+doug9427@users.noreply.github.com>
2021-12-28 20:01:46 -0600

    Add files via upload
    Unsure if we need to add them together by rows or columns.  So I have a code for both a row and column merge.

--- Foster Care System - Final Project.R
+++ Foster Care System - Final Project.R
@@ -32,7 +32,29 @@ Juveniles_USA = Juveniles_IN %>% filter((Location == 'United States') & (TimeFra
 Juveniles_USA = Juveniles_USA %>% mutate(Data = as.numeric(Data))
 Juveniles_USA1 = aggregate(Data~Location, Juveniles_USA, sum)
 
+# Adding blank AgeGroup column and reordering the columns
+Juveniles_USA1$AgeGroup = " "
+Juveniles_usa = Juveniles_USA1[, c(1, 3, 2)]
+
 # Subsetting the 10 States' data
 Juveniles_States = Juveniles_IN %>% filter((Location %in% c('Arizona', 'California', 'Colorado', 'Connecticut', 'Georgia', 'Indiana', 'Ohio', 'Texas', 'Utah', 'Vermont')) & (TimeFrame %in% c('2001', '2003', '2006', '2007', '2010', '2011', '2013', '2015', '2017', '2019')))
 Juveniles_States = Juveniles_States %>% mutate(Data = as.numeric(Data))
 Juveniles_States1 = aggregate(Data~Location, Juveniles_States, sum)
+
+# Adding blank AgeGroup column and reordering the columns
+Juveniles_States1$AgeGroup = " "
+Juveniles_states = Juveniles_States1[, c(1, 3, 2)]
+
+## Combine the datasets together by rows
+# USA datesets 
+Foster_USA = rbind(Exiting_USA1, Juveniles_usa)
+
+# States datasets
+Foster_States = rbind(Exiting_States1, Juveniles_states)
+
+## Combine the datasets together by columns
+# USA datasets
+Foster_USA1 = merge(Exiting_USA1, Juveniles_usa, by=c('Location'))
+
+# States datasets
+Foster_States1 = merge(Exiting_States1, Juveniles_states, by=c('Location'))
