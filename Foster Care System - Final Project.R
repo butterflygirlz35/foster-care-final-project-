0868ae4d7e02f1a8f0f9638164257930df14b444
doug9427 <95145385+doug9427@users.noreply.github.com>
2022-01-11 10:41:45 -0600

    Add files via upload

--- Foster Care System - Final Project.R
+++ Foster Care System - Final Project.R
@@ -1,35 +1,34 @@
 ### Loading libraries
 library(dplyr)
 library(tidyr)
-
+library(rcompanion)
 
 ### Data Wrangling
 ## Exiting the Foster Care System dataset
 Exiting_FC = Children_exiting_foster_care_by_age_group
-Exiting_FC = na.omit(Exiting_FC)
 Exiting_FC = filter(Exiting_FC, DataFormat != 'Percent')
+Exiting_FC = Exiting_FC %>% mutate(Data = as.numeric(Data))
 Exiting_fc = select(Exiting_FC, c('Location', 'Age group', 'TimeFrame', 'Data'))
+Exiting_fc1 = Exiting_fc %>% filter(Location != 'United States') # Using for Tableau
 Exiting_age = names(Exiting_fc)[names(Exiting_fc) == "Age group"] <- "AgeGroup"
 
 # Subsetting United States' Total data
 Exiting_USA = Exiting_fc %>% filter((Location == 'United States') & (AgeGroup != 'Total') & (TimeFrame %in% c('2001', '2003', '2006', '2007', '2010', '2011', '2013', '2015', '2017', '2019')))
-Exiting_USA = Exiting_USA %>% mutate(Data = as.numeric(Data))
 Exiting_USA1 = aggregate(Data~Location + AgeGroup, Exiting_USA, sum)
 
 # Subsetting the 10 States' data
 Exiting_States = Exiting_fc %>% filter((Location %in% c('Arizona', 'California', 'Colorado', 'Connecticut', 'Georgia', 'Indiana', 'Ohio', 'Texas', 'Utah', 'Vermont')) & (AgeGroup == 'Total') & (TimeFrame %in% c('2001', '2003', '2006', '2007', '2010', '2011', '2013', '2015', '2017', '2019')))
-Exiting_States = Exiting_States %>% mutate(Data = as.numeric(Data))
 Exiting_States1 = aggregate(Data~Location + AgeGroup, Exiting_States, sum)
 
 ## Youth incarceration dataset
 Juveniles = Youth_residing_in_juvenile_detention_correctional_and_or_residential_facilities
-Juveniles = na.omit(Juveniles)
+Juveniles = Juveniles %>% mutate(Data = as.numeric(Data))
 Juveniles = filter(Juveniles, DataFormat != 'Rate per 100,000')
 Juveniles_IN = select(Juveniles, c('Location', 'TimeFrame', 'Data'))
+Juveniles_IN = Juveniles_IN %>% mutate(Data = as.numeric(Data))
 
 # Subsetting United States' Total data
 Juveniles_USA = Juveniles_IN %>% filter((Location == 'United States') & (TimeFrame %in% c('2001', '2003', '2006', '2007', '2010', '2011', '2013', '2015', '2017', '2019')))
-Juveniles_USA = Juveniles_USA %>% mutate(Data = as.numeric(Data))
 Juveniles_USA1 = aggregate(Data~Location, Juveniles_USA, sum)
 
 # Adding blank AgeGroup column and reordering the columns
@@ -58,3 +57,5 @@ Foster_USA1 = merge(Exiting_USA1, Juveniles_usa, by=c('Location'))
 
 # States datasets
 Foster_States1 = merge(Exiting_States1, Juveniles_states, by=c('Location'))
+
+
